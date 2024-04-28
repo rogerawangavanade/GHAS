d90b6546de96b71bb943e11bbbcad79934d1e352
rogerawangavanade <145726897+rogerawangavanade@users.noreply.github.com>
2024-04-28 11:31:55 +1000

    Fixed Incorrect suffix

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -17,13 +17,6 @@ function App({ instance }) {
     );
 }
 
-function endsWith(x, y) {
-
-    let index = x.lastIndexOf(y);
-    return x.lastIndexOf(y) === x.length - y.length;
-
-}
-
 const Pages = () => {
     return (
         <Routes>
--- webapi/obj/Debug/net7.0/webapi.AssemblyInfo.cs
+++ webapi/obj/Debug/net7.0/webapi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webapi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+996ae5a7a989f03c44ea9e7b1ad4ae23b5fb150b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03440fc511a9f7101f6b72873f837bcc3218640b")]
 [assembly: System.Reflection.AssemblyProductAttribute("webapi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webapi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
